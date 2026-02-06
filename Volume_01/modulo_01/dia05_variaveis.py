7eb71678876b91b4303b460a76b3c76d5bfd1517
Aryman Garcia Reis <arymanreis@gmail.com>
2026-02-05 21:17:41 -0300

    feat: Conclusao do Modulo 0 e inicio do modulo 1 Dia 05 (Variaveis)

---
+++ Volume_01/modulo_01/dia05_variaveis.py
@@ -0,0 +1,9 @@
+# Criando as variaveis (Etiquetando as caixas)
+fazenda = "Santa Inês"
+sacas = 50
+preco = 1200.50
+
+# Mostrando o que tem dentro (O comando print)
+print(fazenda)
+print(sacas)
+print(preco)
