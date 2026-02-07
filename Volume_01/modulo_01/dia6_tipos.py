c9800a9f93b6841b2f805004f588a87bb01d7e1a
Aryman Garcia Reis <arymanreis@gmail.com>
2026-02-06 22:25:00 -0300

    feat: Modulo 1 Dia 06 Tipos de dados

---
+++ Volume_01/modulo_01/dia6_tipos.py
@@ -0,0 +1,11 @@
+# Declarando os tipos
+fazenda = "Santa Inês"    # String (Texto)
+sacas = 50                # Int (Inteiro)
+preco_medio = 1200.50     # Float (Decimal)
+tem_cafe = True           # Boolean (Verdadeiro/Falso)
+
+# O Raio-X (Verificando a natureza)
+print(type(fazenda))
+print(type(sacas))
+print(type(preco_medio))
+print(type(tem_cafe))
